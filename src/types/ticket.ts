510b211193510a65c62e8df9a4745e8ca65d4144
Gilad Grant <ggrant966@gmail.com>
2021-07-29 09:38:17 +0100

    add ticket type

---
+++ src/types/ticket.ts
@@ -0,0 +1,19 @@
+export type TApiTicket = {
+  id: number;
+  name: string;
+  allowBus: boolean;
+  allowMetro: boolean;
+  allowTrain: boolean;
+  ticketCurrentAmount: number;
+  validity: string;
+  // Direct debit details
+  buyOnDirectDebit: boolean;
+  directDebitCode: string;
+};
+
+export type TTicket = {
+  id: number;
+  name: string;
+  priceString: string;
+  modes: Array<'bus' | 'rail' | 'metro'>;
+};
