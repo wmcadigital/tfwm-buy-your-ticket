e1d9f5cc89d0b16ce0448423f16f4fb8b21f3a8a
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-08-03 15:38:19 +0100

    Adding Radios components

---
+++ src/components/shared/Radios/Radio/Radio.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import dompurify from 'dompurify';
+
+// Import contexts
+// import { useFormContext, TForm } from 'globalState';
+
+import s from './Radio.module.scss';
+
+const { sanitize } = dompurify;
+
+export type RadioProps = {
+  text: string;
+  value: string;
+  info: string;
+  name: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radio = ({ name, onChange, text, value, info }: RadioProps) => {
+  // const [formState] = useFormContext(); // Get the state/dispatch of form data from FormContext
+  // const defaultChecked = formState.ticketInfo[name];
+
+  return (
+    <>
+      <label className={`${s.radioContainer} wmnds-fe-radios__container`}>
+        <div className={s.text} dangerouslySetInnerHTML={{ __html: sanitize(text) }} />
+        <input
+          className={`${s.radio} wmnds-fe-radios__input`}
+          value={value}
+          name={name}
+          type="radio"
+          onChange={onChange}
+          // defaultChecked={defaultChecked === value}
+        />
+        {info && (
+          <div
+            className={`${s.insetText} wmnds-inset-text wmnds-m-t-xs`}
+            dangerouslySetInnerHTML={{ __html: sanitize(info) }}
+          />
+        )}
+        <span className="wmnds-fe-radios__checkmark" />
+      </label>
+    </>
+  );
+};
+
+export default Radio;
---
+++ src/components/shared/Radios/Radios.tsx
@@ -0,0 +1,54 @@
+import dompurify from 'dompurify';
+// import { TForm } from 'globalState';
+// Import components
+import Radio from './Radio/Radio';
+
+import s from './Radios.module.scss';
+
+const { sanitize } = dompurify;
+
+type RadiosProps = {
+  name: string;
+  hint?: string;
+  question: string;
+  error: { message: string } | null;
+  radios: { text: string; html: string; value: string; info: string }[];
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const Radios = ({ name, hint, question, error, radios, onChange }: RadiosProps) => {
+  return (
+    <div className="wmnds-fe-group wmnds-m-b-md">
+      <fieldset className="wmnds-fe-fieldset">
+        <legend className="wmnds-fe-fieldset__legend">
+          <h2 className="wmnds-fe-question">{question}</h2>
+          {hint && <p className={s.hint}>{hint}</p>}
+        </legend>
+        <div className={`wmnds-fe-radios${error ? ' wmnds-fe-group--error' : ''}`}>
+          {/* If there is an error, show here */}
+          {error && (
+            <span
+              className="wmnds-fe-error-message"
+              dangerouslySetInnerHTML={{
+                __html: sanitize(error.message),
+              }}
+            />
+          )}
+          {/* Loop through radios and display each radio button */}
+          {radios.map(({ text, html, value, info }) => (
+            <Radio
+              key={text}
+              name={name}
+              text={html || text}
+              value={value}
+              onChange={onChange}
+              info={info}
+            />
+          ))}
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Radios;
