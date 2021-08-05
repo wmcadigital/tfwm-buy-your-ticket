04bea6c668c57e326be29c4608cceab223b256c4
Gilad Grant <ggrant966@gmail.com>
2021-08-05 16:14:42 +0100

    add formData state to component

--- src/components/App/Form/Sections/SectionOne/Steps/AddToExistingSwiftCard.tsx
+++ src/components/App/Form/Sections/SectionOne/Steps/AddToExistingSwiftCard.tsx
@@ -1,17 +1,30 @@
 import QuestionCard from 'components/App/Form/QuestionCard/QuestionCard';
 import Radios from 'components/shared/Radios/Radios';
-// import useFormDataSubscription from 'customHooks/useFormDataSubscription';
+import useFormDataSubscription from 'customHooks/useFormDataSubscription';
 import { TStepProps } from 'types/step';
+import { convertYesNoToBoolean, convertBooleanToYesNo } from 'helpers/yesNoBoolean';
 
 const AddToExistingSwiftCard = ({ stepNavigation }: TStepProps) => {
-  const { goToNextStep } = stepNavigation;
-  // const subscriptions = useFormDataSubscription(['ticketHolderCurrentTown']);
-  // const [ticketHolderCurrentTown] = subscriptions;
+  const { goToNextStep, skipToStep } = stepNavigation;
+
+  const addProductToExistingCard = useFormDataSubscription('addProductToExistingCard');
+  const { value } = addProductToExistingCard;
+
+  const currentValue = value === null ? null : convertBooleanToYesNo(value);
+  const setCurrentValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    addProductToExistingCard.set(convertYesNoToBoolean(e.target.value as 'yes' | 'no'));
+  };
+
+  const handleContinue = () => {
+    addProductToExistingCard.save();
+    if (value === true) return goToNextStep();
+    return skipToStep(4);
+  };
 
   return (
     <QuestionCard
       question="Would you like to add the ticket to an existing Swift card?"
-      handleContinue={goToNextStep}
+      handleContinue={handleContinue}
     >
       <Radios
         name="swiftCard"
@@ -27,6 +40,8 @@ const AddToExistingSwiftCard = ({ stepNavigation }: TStepProps) => {
           </>
         }
         error={null}
+        currentValue={currentValue}
+        onChange={setCurrentValue}
         radios={[
           { text: 'Yes', html: null, value: 'yes', info: null },
           { text: 'No, I need a new card', html: null, value: 'no', info: null },
