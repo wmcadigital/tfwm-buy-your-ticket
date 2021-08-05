318267f1eee7939a571ebc9d18e4f371fae752fe
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-08-05 10:32:42 +0100

    Update step 2: check if there is an existing swift card.

--- src/components/App/Form/Sections/SectionOne/Steps/AddToExistingSwiftCard.tsx
+++ src/components/App/Form/Sections/SectionOne/Steps/AddToExistingSwiftCard.tsx
@@ -1,36 +1,37 @@
-import Button from 'components/shared/Button';
-import useFormDataSubscription from 'customHooks/useFormDataSubscription';
+import QuestionCard from 'components/App/Form/QuestionCard/QuestionCard';
+import Radios from 'components/shared/Radios/Radios';
+// import useFormDataSubscription from 'customHooks/useFormDataSubscription';
 import { TStepProps } from 'types/step';
 
 const AddToExistingSwiftCard = ({ goToNextStep }: TStepProps) => {
-  const subscriptions = useFormDataSubscription(['ticketHolderCurrentTown']);
-  const [ticketHolderCurrentTown] = subscriptions;
+  // const subscriptions = useFormDataSubscription(['ticketHolderCurrentTown']);
+  // const [ticketHolderCurrentTown] = subscriptions;
 
   return (
-    <>
-      <h2 className="wmnds-m-t-lg">Would you like to add the ticket to an existing Swift card?</h2>
-      <p>Your Swift card needs to:</p>
-      <ul>
-        <li>have a photo on</li>
-        <li>
-          begin with <strong>633597 0107</strong>
-        </li>
-      </ul>
-      <div className="wmnds-fe-group">
-        <label className="wmnds-fe-label" htmlFor="input">
-          Current town
-        </label>
-        <input
-          className="wmnds-fe-input"
-          id="input"
-          name="input"
-          type="text"
-          value={(ticketHolderCurrentTown.value as string | number) || ''}
-          onChange={(e) => ticketHolderCurrentTown.set(e.target.value)}
-        />
-      </div>
-      <Button type="button" text="Continue" onClick={goToNextStep} />
-    </>
+    <QuestionCard
+      question="Would you like to add the ticket to an existing Swift card?"
+      handleContinue={goToNextStep}
+    >
+      <Radios
+        name="swiftCard"
+        hint={
+          <>
+            <p>Your Swift card needs to:</p>
+            <ul>
+              <li>have a photo on</li>
+              <li>
+                begin with <strong>633597 0107</strong>
+              </li>
+            </ul>
+          </>
+        }
+        error={null}
+        radios={[
+          { text: 'Yes', html: null, value: 'yes', info: null },
+          { text: 'No, I need a new card', html: null, value: 'no', info: null },
+        ]}
+      />
+    </QuestionCard>
   );
 };
 
