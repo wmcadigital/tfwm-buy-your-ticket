114600e47d3607e6b412d10e68a4a67d685a2ca7
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-08-06 20:13:35 +0100

    Update section 1 steps

--- src/components/App/Form/Sections/SectionOne/Steps/AddSwiftCardNumber.tsx
+++ src/components/App/Form/Sections/SectionOne/Steps/AddSwiftCardNumber.tsx
@@ -1,19 +1,32 @@
 import QuestionCard from 'components/App/Form/QuestionCard/QuestionCard';
 import Input from 'components/shared/Input';
 import InsetText from 'components/shared/InsetText/InsetText';
-// import useFormDataSubscription from 'customHooks/useFormDataSubscription';
+import useFormDataSubscription from 'customHooks/useFormDataSubscription';
 import { TStepProps } from 'types/step';
 
 const AddSwiftCard = ({ stepNavigation }: TStepProps) => {
   const { goToNextStep } = stepNavigation;
-  // currentSwiftcardNumber
+
+  const currentSwiftcardNumber = useFormDataSubscription('currentSwiftcardNumber');
+  const { value } = currentSwiftcardNumber;
+
+  const setCurrentValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    currentSwiftcardNumber.set(e.target.value);
+  };
+
+  const handleContinue = () => {
+    currentSwiftcardNumber.save();
+    goToNextStep();
+  };
 
   return (
-    <QuestionCard question="What is the Swift card number?" handleContinue={goToNextStep}>
+    <QuestionCard question="What is the Swift card number?" handleContinue={handleContinue}>
       <Input
         name="swiftCardNumber"
         label="This is the 16-digit number on the front of the card."
         className="wmnds-col-1 wmnds-col-md-2-3"
+        onChange={setCurrentValue}
+        defaultValue={value}
       />
       <InsetText
         classes="wmnds-m-b-lg"
--- src/components/App/Form/Sections/SectionOne/Steps/CheckIfUserIsTheTicketHolder.tsx
+++ src/components/App/Form/Sections/SectionOne/Steps/CheckIfUserIsTheTicketHolder.tsx
@@ -1,27 +1,40 @@
 import QuestionCard from 'components/App/Form/QuestionCard/QuestionCard';
 import Radios from 'components/shared/Radios/Radios';
+import { convertYesNoToBoolean, convertBooleanToYesNo } from 'helpers/yesNoBoolean';
 
-// import useFormDataSubscription from 'customHooks/useFormDataSubscription';
+import useFormDataSubscription from 'customHooks/useFormDataSubscription';
 import { TStepProps } from 'types/step';
 
 const CheckIfUserIsTheTicketHolder = ({ stepNavigation }: TStepProps) => {
-  const { goToNextSection } = stepNavigation;
-  // const applicationForMe = useFormDataSubscription('applicationForMe');
+  const { goToNextStep, skipToSection } = stepNavigation;
 
-  // applicationForMe.set(false);
-  // applicationForMe.save();
+  const applicationForMe = useFormDataSubscription('applicationForMe');
+  const { value } = applicationForMe;
+
+  const currentValue = value === null ? null : convertBooleanToYesNo(value);
+  const setCurrentValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    applicationForMe.set(convertYesNoToBoolean(e.target.value as 'yes' | 'no'));
+  };
+
+  const handleContinue = () => {
+    applicationForMe.save();
+    if (!value) return goToNextStep();
+    return skipToSection(3);
+  };
 
   return (
     <QuestionCard
       question="Are you buying the ticket for yourself?"
-      handleContinue={goToNextSection}
+      handleContinue={handleContinue}
     >
       <Radios
-        name=""
+        name="isApplicationForMe"
+        onChange={setCurrentValue}
+        currentValue={currentValue}
         error={null}
         radios={[
-          { text: 'Yes', html: null, value: 'true', info: null },
-          { text: 'No', html: null, value: 'false', info: null },
+          { text: 'Yes', html: null, value: 'yes', info: null },
+          { text: 'No', html: null, value: 'no', info: null },
         ]}
       />
     </QuestionCard>
