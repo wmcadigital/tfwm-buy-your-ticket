59a6096dcf59327247c733ef39aec018473e6f8f
Catia Costa <catia.rodrigues.costa@gmail.com>
2021-08-11 13:54:38 +0100

    Add generic error component as an option to be shown inside QuestionCard.

--- src/components/App/Form/QuestionCard/QuestionCard.tsx
+++ src/components/App/Form/QuestionCard/QuestionCard.tsx
@@ -1,16 +1,20 @@
 import Button from 'components/shared/Button';
+import GenericError from 'components/shared/Errors/GenericError';
 
 const QuestionCard = ({
   question,
   handleContinue,
   children,
+  showError,
 }: {
   question: string;
   handleContinue: () => void;
   children: React.ReactNode;
+  showError?: boolean;
 }): JSX.Element => {
   return (
     <>
+      {showError && <GenericError />}
       <h2 className="wmnds-m-t-lg wmnds-m-b-lg">{question}</h2>
       {children}
       <div className="wmnds-col-1">
