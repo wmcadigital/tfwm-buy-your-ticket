4392ccbd4b19006b23f9893f594d51f860a8aa13
Gilad Grant <ggrant966@gmail.com>
2021-08-25 11:28:36 +0100

    fix propTypes error

--- src/components/shared/Radios/Radios.tsx
+++ src/components/shared/Radios/Radios.tsx
@@ -47,7 +47,7 @@ Radios.propTypes = {
   hint: PropTypes.node,
   error: PropTypes.objectOf(PropTypes.objectOf(PropTypes.string)),
   onChange: PropTypes.func.isRequired,
-  currentValue: PropTypes.oneOf([PropTypes.string, PropTypes.bool]),
+  currentValue: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
   required: PropTypes.bool,
 };
 
