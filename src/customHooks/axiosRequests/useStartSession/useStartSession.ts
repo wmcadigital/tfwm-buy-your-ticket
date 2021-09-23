2a86d3edc71c32a3989eccb7fa0bff3c6cc504c3
Gilad Grant <ggrant966@gmail.com>
2021-09-23 14:04:15 +0100

    fix multiple sessions bug

--- src/customHooks/axiosRequests/useStartSession/useStartSession.ts
+++ src/customHooks/axiosRequests/useStartSession/useStartSession.ts
@@ -1,15 +1,31 @@
 import { TSession } from 'types/session';
-
+import { useGlobalContext } from 'state/globalState/context';
+import { AxiosResponse } from 'axios';
 import useAxiosRequest from '../_useAxiosRequest';
 import { TuseStartSession } from './useStartSession.types';
 
 const useStartSession: TuseStartSession = () => {
   const { REACT_APP_DBAPI_HOST } = process.env;
 
+  const [globalState] = useGlobalContext();
+  const { session } = globalState;
+
   const startSessionRequest = useAxiosRequest<TSession>({
     url: `${REACT_APP_DBAPI_HOST}/DirectDebit/StartSession`,
     method: 'POST',
   });
+
+  if (session.sessionNo && session.id) {
+    return {
+      startSession: () => {
+        return (Promise.resolve({ data: { session } }) as unknown) as Promise<
+          AxiosResponse<TSession>
+        >;
+      },
+      isLoading: false,
+      hasError: false,
+    };
+  }
 
   const { isLoading, sendRequest, hasError } = startSessionRequest;
 
